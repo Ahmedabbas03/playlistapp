72c13615dbae5197ce43a2603ffe11c54bc332ab
appdev <abbasah@oregonstate.edu>
2023-12-04 16:22:03 -0600

    Intergrating with Microservice

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,7 +14,7 @@ const loginUser = async (req, res) => {
 
     const token = createToken(user._id);
 
-    res.status(200).json({ name: user.name, email, token });
+    res.status(200).json({ name: user.name, email, token, _id: user._id });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
